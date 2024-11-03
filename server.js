422dd572f9a1b4942e170ab5761def9598a4537e
Leonardo Becker <leonardo.becker@sou.unijui.edu.br>
2024-11-03 19:44:24 -0300

    versao 2

--- server.js
+++ server.js
@@ -1,71 +1,63 @@
-import express from 'express'
-import { PrismaClient } from '@prisma/client'
-import cors from 'cors'
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const app = express() // Iniciando o express
-app.use(express.json()) // Dizendo que o express vai usar o json
-app.use(cors())
+const app = express(); // Iniciando o express
+app.use(express.json()); // Dizendo que o express vai usar o json
+app.use(cors());
 
 // Rota de criação
-app.post('/turmas', async (req, res) => {
-    await prisma.turmas.create({
-        data: {
-            title: req.body.title,
-            subtitle: req.body.subtitle,
-            teacher: req.body.teacher,
-            pfp: req.body.pfp,
-            bg: req.body.bg
-        }
-    })
+app.post("/tarefas", async (req, res) => {
+  await prisma.tarefas.create({
+    data: {
+      tarefa: req.body.tarefa,
+      prioridade: req.body.prioridade,
+    },
+  });
 
-    res.status(201).json(req.body)
-})
+  res.status(201).json(req.body);
+});
 
 // Rota de listagem
-app.get('/turmas', async (req, res) => {
+app.get("/tarefas", async (req, res) => {
+  let tarefas = [];
 
-    let turmas = []
-
-    if (req.query) {
-        turmas = await prisma.turmas.findMany({
-            where: {
-                id: req.query.id
-            }
-        })
-
-    } else {
-        turmas = await prisma.turmas.findMany()
-    }
-    res.status(200).json(turmas)
-})
+  if (req.query) {
+    tarefas = await prisma.tarefas.findMany({
+      where: {
+        id: req.query.id,
+      },
+    });
+  } else {
+    tarefas = await prisma.tarefas.findMany();
+  }
+  res.status(200).json(tarefas);
+});
 
 // Rota de edição
-app.put('/turmas/:id', async (req, res) => {
-    await prisma.turmas.update({
-        where: {
-            id: req.params.id
-        },
-        data: {
-            title: req.body.title,
-            subtitle: req.body.subtitle,
-            teacher: req.body.teacher,
-            pfp: req.body.pfp,
-            bg: req.body.bg
-        }
-    })
-    res.status(200).json(req.body)
-})
+app.put("/tarefas/:id", async (req, res) => {
+  await prisma.tarefas.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      tarefa: req.body.tarefa,
+      prioridade: req.body.prioridade,
+    },
+  });
+  res.status(200).json(req.body);
+});
 
 // Rota de delecão
-app.delete('/turmas/:id', async (req, res) => {
-    await prisma.turmas.delete({
-        where: {
-            id: req.params.id
-        }
-    })
-    res.status(200).json({ message: 'Usuario deletado com sucesso!' })
-})
+app.delete("/tarefas/:id", async (req, res) => {
+  await prisma.tarefas.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json({ message: "Tarefa deletada com sucesso!" });
+});
 
-app.listen(3000)
+app.listen(3000);
